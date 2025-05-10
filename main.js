d0b6e8806d6805a35ec770ccf2af13165d905832
mohsinju99 <mohsinju@sheridancollege.ca>
2025-05-10 19:17:42 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+
+});
+
+function toggleFretboardVisibility() {
+    document.getElementById("fretboard-div").classList.toggle("hidden");
+}
+
+function toggleChordSearchVisibility() {
+    document.getElementById("chord-search-div").classList.toggle("hidden");
+}
