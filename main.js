1bd4d7bd6c95e867dceb62f5655d16337108630a
mohsinju99 <mohsinju@sheridancollege.ca>
2025-05-12 09:16:15 -0400

    chord search form created, api not working yet

--- main.js
+++ main.js
@@ -6,24 +6,44 @@ let num_strings = 6;
 let num_frets = 10;
 let fretboard = generateFretboard();
 
+let chord_types = ["maj", "min", "dim"];
+
 document.addEventListener("DOMContentLoaded", (event) => {
     
     //generateFretboard();
 
-    createFretboardDisplay(num_frets, num_strings);
+    createFretboardDisplay();
+    createChordSearchForm();
 });
 
-function toggleFretboardVisibility()
+function showFretboard()
 {
-    document.getElementById("fretboard-div").classList.toggle("hidden");
-    document.getElementById("chord-search-div").classList.toggle("hidden");
+    hideChordSearch();
+
+    const fretboard_div = document.getElementById("fretboard-div");
+    fretboard_div.style.display = "flex";
 }
 
-function toggleChordSearchVisibility()
+function hideFretboard()
 {
-    document.getElementById("chord-search-div").classList.toggle("hidden");
-    document.getElementById("fretboard-div").classList.toggle("hidden");
+    const fretboard_div = document.getElementById("fretboard-div");
+    fretboard_div.style.display = "none";
 }
+
+function showChordSearch()
+{
+    hideFretboard();
+
+    const chord_search_div = document.getElementById("chord-search-div");
+    chord_search_div.style.display = "grid";
+}
+
+function hideChordSearch()
+{
+    const chord_search_div = document.getElementById("chord-search-div");
+    chord_search_div.style.display = "none"; 
+}
+
 
 function generateFretboard()
 {
@@ -62,8 +82,8 @@ function generateFretboard()
 
 function createFretboardDisplay()
 {
-    //const fretboard = document.getElementById("fretboard-div");
-    const fretboard_display = document.createDocumentFragment();
+    const fretboard_display = document.createElement('div');
+    fretboard_display.id = "fretboard-display";
 
     for (let s = 0; s < num_strings; s++)
     {
@@ -106,7 +126,82 @@ function createFretboardDisplay()
     document.getElementById("fretboard-div").appendChild(fretboard_display);
 }
 
-function toggleNote(event)
+function toggleNote()
 {
     document.getElementById(this.id).childNodes[0].classList.toggle("hidden");
+}
+
+function createChordSearchForm() 
+{
+    const chord_search_display = document.createDocumentFragment();
+    const form = document.createElement('form');
+
+    const note_select = document.createElement('select');
+    note_select.id = "noteSelect";
+    
+    for (let i = 0; i < notes.length; i++)
+    {
+        const new_option = document.createElement('option');
+        new_option.text = notes[i];
+        new_option.value = notes[i];
+
+        note_select.options.add(new_option);
+    }
+
+    form.appendChild(note_select);
+
+    const chord_type_select = document.createElement('select');
+    chord_type_select.id = "chordTypeSelect";
+
+    for (let i = 0; i < chord_types.length; i++)
+    {
+        const new_option = document.createElement('option');
+        new_option.text = chord_types[i];
+        new_option.value = chord_types[i];
+
+        chord_type_select.options.add(new_option); 
+    }
+
+    form.appendChild(chord_type_select);
+
+    const form_submit_btn = document.createElement('input');
+    form_submit_btn.type = "submit";
+    form_submit_btn.text = "Search";
+    form_submit_btn.id  = "form-submit-btn";
+
+    form.appendChild(form_submit_btn);
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const form = e.target;
+        
+        const chord = form.noteSelect.value + form.chordTypeSelect.value;
+
+        getChord(chord);
+    });
+
+    chord_search_display.appendChild(form);
+
+    const image_container = document.createElement('div');
+    image_container.id = "image-container";
+
+    chord_search_display.appendChild(image_container);
+
+    document.getElementById("chord-search-div").appendChild(chord_search_display);
+    
+}
+
+function getChord(chord)
+{
+    const output_img = document.createElement('ins');
+
+    output_img.className = "scales_chords_api";
+    output_img.setAttribute("chord", chord);
+
+    document.getElementById("image-container").appendChild(output_img);
+
+    console.log("Form Submitted");
+    console.log(chord);
+    console.log(output_img);
 }
